e95cbf36341509254bfc08a61c419d17b80f4b0d
hiro-tar <hirot@jp.ibm.com>
2016-07-23 23:02:03 +0900

    Update SimpleServlet.java
    Add a message.

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -20,7 +20,7 @@ public class SimpleServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html<BR/> Bounjour Monde !");
         response.getWriter().print("Hello World!");
     }
 
